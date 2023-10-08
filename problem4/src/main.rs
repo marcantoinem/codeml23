3e0731661a041145ce7495b3c7820255517a7355
lemark <marcantoine_manningham@outlook.com>
2023-10-08 01:11:07 -0400

    Even more things working

--- problem4/src/main.rs
+++ problem4/src/main.rs
@@ -1,51 +1,104 @@
 pub mod preprocessing;
 
-use ndarray_rand::rand::SeedableRng;
-use rand::rngs::SmallRng;
-
 use crate::preprocessing::{parse_rows, result};
-use linfa::prelude::*;
-use linfa_svm::{error::Result, Svm};
+use smartcore::{
+    ensemble::random_forest_classifier::{
+        RandomForestClassifier, RandomForestClassifierParameters,
+    },
+    linear::logistic_regression::{LogisticRegression, LogisticRegressionParameters},
+    naive_bayes::{
+        bernoulli::{BernoulliNB, BernoulliNBParameters},
+        gaussian::{GaussianNB, GaussianNBParameters},
+    },
+    neighbors::knn_classifier::{KNNClassifier, KNNClassifierParameters},
+    tree::decision_tree_classifier::{DecisionTreeClassifier, DecisionTreeClassifierParameters},
+};
+use std::error::Error;
+// DenseMatrix definition
+use smartcore::linalg::basic::matrix::DenseMatrix;
+// KNNClassifier
 
-fn main() -> Result<()> {
-    let mut rng = SmallRng::seed_from_u64(42);
+// Various distance metrics
 
-    let (train, test) = parse_rows().shuffle(&mut rng).split_with_ratio(0.9);
+fn split<T: Clone>(test: Vec<T>, ratio: f64) -> (Vec<T>, Vec<T>) {
+    let (x1, x2) = test.split_at((ratio * test.len() as f64) as usize);
+    (x1.iter().cloned().collect(), x2.iter().cloned().collect())
+}
 
-    println!("Training model with Gini criterion ...");
-    let model = Svm::<_, bool>::params()
-        .pos_neg_weights(10., 10.)
-        .gaussian_kernel(80.0)
-        .fit(&train)?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let (x, y) = parse_rows();
+    let (x, x_test) = split(x, 0.90);
+    let (y, y_test) = split(y, 0.90);
+    let x = DenseMatrix::from_2d_vec(&x);
+    let x_test = DenseMatrix::from_2d_vec(&x_test);
 
-    let gini_pred_y = model.predict(&test);
-    let cm = gini_pred_y.confusion_matrix(&test)?;
+    let parameters = LogisticRegressionParameters::default();
 
-    let to_predict = model.predict(&result());
+    let tree = LogisticRegression::fit(&x, &y, parameters).unwrap();
+
+    let next_predict = DenseMatrix::from_2d_vec(&result());
+    let to_predict = tree.predict(&next_predict).unwrap();
+
+    let y_test_result = tree.predict(&x_test).unwrap();
+    let y_test_result: Vec<u8> = y_test_result.into_iter().map(|c| (c >= 1) as u8).collect();
+    println!("{:?}", &y_test[0..42]);
+    println!("{:?}", &y_test_result[0..42]);
+    let true_positive: u16 = y_test
+        .iter()
+        .zip(&y_test_result)
+        .map(|(&x, &y)| (x == y && x != 0) as u16)
+        .sum();
+
+    let false_negative: u16 = y_test
+        .iter()
+        .zip(&y_test_result)
+        .map(|(&x, &y)| (x != y && y == 0) as u16)
+        .sum();
+
+    let false_positive: u16 = y_test
+        .iter()
+        .zip(&y_test_result)
+        .map(|(&x, &y)| (x != y && y != 0) as u16)
+        .sum();
+
+    let true_negative: u16 = y_test
+        .iter()
+        .zip(&y_test_result)
+        .map(|(&x, &y)| (x == y && x == 0) as u16)
+        .sum();
+    println!(
+        "{} {} {} {}",
+        true_positive, true_negative, false_positive, false_negative
+    );
+
+    let total = true_negative + false_positive + false_negative + true_positive;
+    println!(
+        "Accuracy: {}",
+        (true_positive + true_negative) as f64 / total as f64
+    );
+
+    println!(
+        "F1 score {}",
+        (2.0 * true_positive as f64)
+            / (2.0 * true_positive as f64 + false_positive as f64 + false_negative as f64)
+    );
+
     let mut string = String::new();
     let mut lines = include_str!("../desjardins_presubmissions.csv").lines();
+
     lines.next();
     string.push_str(&("ID,default-payment-next-month\n"));
     for (row, value) in lines.zip(to_predict) {
         let mut tests = row.split(',');
         tests.next();
         let id = tests.next().unwrap();
-        if value {
+        if value == 1 {
             string.push_str(&format!("{},1\n", id));
         } else {
             string.push_str(&format!("{},0\n", id));
         }
     }
     std::fs::write("test.csv", string).unwrap();
-
-    println!("{:?}", cm);
-
-    println!(
-        "Test accuracy with Gini criterion: {:.2}%",
-        100.0 * cm.accuracy()
-    );
-
-    // let gini_pred_y = gini_model.predict();
 
     Ok(())
 }
--- problem4/src/preprocessing.rs
+++ problem4/src/preprocessing.rs
@@ -1,84 +1,63 @@
-use linfa::Dataset;
-use ndarray::{Array, Array1, Array2, Ix1};
-use std::{array, collections::HashMap};
+use std::array;
 
-enum Education {
-    University,
-    GraduateSchool,
-    HighSchool,
-}
-
-impl Education {
-    fn from(education: &str) -> Self {
-        match education {
-            "university" => Self::University,
-            "graduate school" => Self::GraduateSchool,
-            "high school" => Self::HighSchool,
-            _ => Self::HighSchool,
-        }
+fn education(education: &str) -> f64 {
+    match education {
+        "university" => 3.0,
+        "graduate school" => 2.0,
+        "high school" => 1.0,
+        _ => 1.0,
     }
 }
 
-enum Status {
-    Married,
-    Single,
-}
-
-impl Status {
-    fn from(status: &str) -> Self {
-        match status {
-            "married" => Self::Married,
-            "single" => Self::Single,
-            _ => Self::Single,
-        }
+fn status(status: &str) -> f64 {
+    match status {
+        "married" => 1.0,
+        "single" => 0.0,
+        _ => 0.0,
     }
 }
 
-fn parse_row(row: &str) -> (Education, Status, [f64; 19], Option<bool>) {
-    let mut result = [0.0; 19];
+fn parse_row(row: &str) -> ([f64; 22], Option<u8>) {
+    let mut result = [0.0; 22];
     let mut iter = row.split(',');
     iter.next(); //skip id
     result[0] = iter.next().unwrap().parse::<f64>().unwrap();
     iter.next(); //skip sex
-    let education = Education::from(iter.next().unwrap());
-    let status = Status::from(iter.next().unwrap());
-    let other_data: [f64; 18] = array::from_fn(|_| iter.next().unwrap().parse::<f64>().unwrap());
+    result[1] = education(iter.next().unwrap());
+    result[2] = status(iter.next().unwrap());
+    let other_data: [f64; 19] = array::from_fn(|_| iter.next().unwrap().parse::<f64>().unwrap());
     result[3..].copy_from_slice(&other_data);
-    let y = iter
-        .next()
-        .and_then(|x| x.parse::<u8>().ok())
-        .map(|x| x != 0);
-    (education, status, result, y)
+    let y = iter.next().and_then(|x| x.parse::<u8>().ok());
+    (result, y)
 }
 
-pub fn parse_rows() -> Dataset<f64, bool, Ix1> {
+pub fn parse_rows() -> (Vec<Vec<f64>>, Vec<u8>) {
     let file = include_str!("../data_participant.csv");
     let mut lines = file.lines();
     lines.next();
     let mut x = vec![];
     let mut y = vec![];
-    let mut data = HashMap::new();
     for row in lines {
         let result = parse_row(row);
-        if let Some(data) = result.3 {
-            data.
-            // x.extend(&result.0);
+        if let Some(data) = result.1 {
+            x.push(result.0.into_iter().collect());
             y.push(data);
         }
     }
-    let x: Array2<f64> = Array::from_shape_vec((x.len() / 21, 21), x).unwrap();
-    let y: Array1<bool> = Array::from_vec(y);
-    Dataset::new(x, y)
+    (x, y)
 }
 
-pub fn result() -> Array2<f64> {
+pub fn result() -> Vec<Vec<f64>> {
     let file = include_str!("../desjardins_presubmissions.csv");
     let mut lines = file.lines();
     lines.next();
     let mut x = vec![];
     for row in lines {
-        let result = parse_row(row);
-        x.extend(&result.0);
+        let mut row: String = row.chars().skip_while(|&c| c != ',').skip(1).collect();
+        let result = parse_row(&row);
+        if let None = result.1 {
+            x.push(result.0.into_iter().collect());
+        }
     }
-    Array::from_shape_vec((x.len() / 21, 21), x).unwrap()
+    x
 }
