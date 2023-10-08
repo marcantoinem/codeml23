ce3cb698fd98d32ed23b02624b73c98353112d63
lemark <marcantoine_manningham@outlook.com>
2023-10-08 07:52:50 -0400

    Avancement

--- problem4/src/main.rs
+++ problem4/src/main.rs
@@ -1,94 +1,17 @@
 pub mod preprocessing;
 
-use crate::preprocessing::{parse_rows, result};
-use smartcore::{
-    ensemble::random_forest_classifier::{
-        RandomForestClassifier, RandomForestClassifierParameters,
-    },
-    linear::logistic_regression::{LogisticRegression, LogisticRegressionParameters},
-    naive_bayes::{
-        bernoulli::{BernoulliNB, BernoulliNBParameters},
-        gaussian::{GaussianNB, GaussianNBParameters},
-    },
-    neighbors::knn_classifier::{KNNClassifier, KNNClassifierParameters},
-    tree::decision_tree_classifier::{DecisionTreeClassifier, DecisionTreeClassifierParameters},
-};
-use std::error::Error;
-// DenseMatrix definition
-use smartcore::linalg::basic::matrix::DenseMatrix;
-// KNNClassifier
+use crate::preprocessing::{result, Models};
 
-// Various distance metrics
-
-fn split<T: Clone>(test: Vec<T>, ratio: f64) -> (Vec<T>, Vec<T>) {
-    let (x1, x2) = test.split_at((ratio * test.len() as f64) as usize);
-    (x1.iter().cloned().collect(), x2.iter().cloned().collect())
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let (x, y) = parse_rows();
-    let (x, x_test) = split(x, 0.90);
-    let (y, y_test) = split(y, 0.90);
-    let x = DenseMatrix::from_2d_vec(&x);
-    let x_test = DenseMatrix::from_2d_vec(&x_test);
-
-    let parameters = LogisticRegressionParameters::default();
-
-    let tree = LogisticRegression::fit(&x, &y, parameters).unwrap();
-
-    let next_predict = DenseMatrix::from_2d_vec(&result());
-    let to_predict = tree.predict(&next_predict).unwrap();
-
-    let y_test_result = tree.predict(&x_test).unwrap();
-    let y_test_result: Vec<u8> = y_test_result.into_iter().map(|c| (c >= 1) as u8).collect();
-    println!("{:?}", &y_test[0..42]);
-    println!("{:?}", &y_test_result[0..42]);
-    let true_positive: u16 = y_test
-        .iter()
-        .zip(&y_test_result)
-        .map(|(&x, &y)| (x == y && x != 0) as u16)
-        .sum();
-
-    let false_negative: u16 = y_test
-        .iter()
-        .zip(&y_test_result)
-        .map(|(&x, &y)| (x != y && y == 0) as u16)
-        .sum();
-
-    let false_positive: u16 = y_test
-        .iter()
-        .zip(&y_test_result)
-        .map(|(&x, &y)| (x != y && y != 0) as u16)
-        .sum();
-
-    let true_negative: u16 = y_test
-        .iter()
-        .zip(&y_test_result)
-        .map(|(&x, &y)| (x == y && x == 0) as u16)
-        .sum();
-    println!(
-        "{} {} {} {}",
-        true_positive, true_negative, false_positive, false_negative
-    );
-
-    let total = true_negative + false_positive + false_negative + true_positive;
-    println!(
-        "Accuracy: {}",
-        (true_positive + true_negative) as f64 / total as f64
-    );
-
-    println!(
-        "F1 score {}",
-        (2.0 * true_positive as f64)
-            / (2.0 * true_positive as f64 + false_positive as f64 + false_negative as f64)
-    );
+fn main() {
+    let models = Models::from_csv().train();
+    let model_prediction = models.predict(&result());
 
     let mut string = String::new();
     let mut lines = include_str!("../desjardins_presubmissions.csv").lines();
 
     lines.next();
     string.push_str(&("ID,default-payment-next-month\n"));
-    for (row, value) in lines.zip(to_predict) {
+    for (row, value) in lines.zip(model_prediction) {
         let mut tests = row.split(',');
         tests.next();
         let id = tests.next().unwrap();
@@ -99,6 +22,4 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
     std::fs::write("test.csv", string).unwrap();
-
-    Ok(())
 }
--- problem4/src/preprocessing.rs
+++ problem4/src/preprocessing.rs
@@ -1,63 +1,285 @@
+use smartcore::{
+    ensemble::random_forest_classifier::{
+        RandomForestClassifier, RandomForestClassifierParameters,
+    },
+    linalg::basic::matrix::DenseMatrix,
+    naive_bayes::{
+        bernoulli::{BernoulliNB, BernoulliNBParameters},
+        gaussian::{GaussianNB, GaussianNBParameters},
+    },
+    tree::decision_tree_classifier::SplitCriterion,
+};
 use std::array;
 
-fn education(education: &str) -> f64 {
-    match education {
-        "university" => 3.0,
-        "graduate school" => 2.0,
-        "high school" => 1.0,
-        _ => 1.0,
+type DataX = [f64; 20];
+type DataY = u8;
+
+#[derive(Debug, PartialEq)]
+pub enum Education {
+    University,
+    GraduateSchool,
+    HighSchool,
+    Other,
+    Unknown,
+}
+
+impl Education {
+    fn new(education: &str) -> Self {
+        match education {
+            "university" => Self::University,
+            "graduate school" => Self::GraduateSchool,
+            "high school" => Self::HighSchool,
+            _ => Self::Unknown,
+        }
+    }
+    fn to_index(&self) -> usize {
+        match self {
+            Self::University => 0,
+            Self::GraduateSchool => 1,
+            Self::HighSchool => 2,
+            Self::Other => 3,
+            Self::Unknown => 4,
+        }
     }
 }
 
-fn status(status: &str) -> f64 {
-    match status {
-        "married" => 1.0,
-        "single" => 0.0,
-        _ => 0.0,
+#[derive(Debug, PartialEq)]
+pub enum MaritalStatus {
+    Married,
+    Single,
+    Other,
+    Unknown,
+}
+
+impl MaritalStatus {
+    fn new(status: &str) -> Self {
+        match status {
+            "single" => Self::Single,
+            "married" => Self::Married,
+            "other" => Self::Other,
+            _ => Self::Unknown,
+        }
+    }
+    fn to_index(&self) -> usize {
+        match self {
+            Self::Single => 0,
+            Self::Married => 1,
+            Self::Other => 2,
+            Self::Unknown => 3,
+        }
     }
 }
 
-fn parse_row(row: &str) -> ([f64; 22], Option<u8>) {
-    let mut result = [0.0; 22];
+#[derive(Debug)]
+pub struct Models {
+    models: [[Model; 4]; 5],
+}
+
+impl Models {
+    pub fn from_csv() -> Models {
+        let file = include_str!("../data_participant.csv");
+        let mut lines = file.lines();
+        lines.next();
+        let mut models = Models::new();
+        for row in lines {
+            let (education, marital_status, x, y) = parse_row(row);
+            if let Some(y) = y {
+                models.push(education, marital_status, x, y);
+            }
+        }
+        models
+    }
+    fn new() -> Self {
+        Self {
+            models: array::from_fn(|_| array::from_fn(|_| Model::new())),
+        }
+    }
+    fn push(&mut self, education: Education, marital_status: MaritalStatus, x: DataX, y: DataY) {
+        if education != Education::Unknown {
+            if marital_status != MaritalStatus::Unknown {
+                self.models[4][3].push(x, y);
+            }
+            self.models[4][marital_status.to_index()].push(x, y);
+        }
+        self.models[education.to_index()][marital_status.to_index()].push(x, y);
+    }
+    pub fn train(self) -> TrainedModels {
+        let models = self.models.map(|layer| layer.map(Model::train));
+        TrainedModels { models }
+    }
+}
+
+#[derive(Debug)]
+struct Model {
+    data: (Vec<DataX>, Vec<DataY>),
+}
+
+impl Model {
+    fn new() -> Self {
+        Self {
+            data: (vec![], vec![]),
+        }
+    }
+    fn push(&mut self, x: DataX, y: DataY) {
+        self.data.0.push(x);
+        self.data.1.push(y);
+    }
+    fn split_data(&self, ratio: f64) -> ((Vec<Vec<f64>>, Vec<u8>), (Vec<Vec<f64>>, Vec<u8>)) {
+        let training_row = (self.data.0.len() as f64 * ratio) as usize;
+        let (x_train, x_ref) = self.data.0.split_at(training_row);
+        let (x_train, x_ref) = (
+            x_train.iter().map(|a| a.to_vec()).collect(),
+            x_ref.iter().map(|a| a.to_vec()).collect(),
+        );
+        let (y_train, y_ref) = self.data.1.split_at(training_row);
+        let (y_train, y_ref) = (
+            y_train.iter().copied().collect(),
+            y_ref.iter().copied().collect(),
+        );
+        ((x_train, y_train), (x_ref, y_ref))
+    }
+    fn train(self) -> TrainedModel {
+        if self.data.0.len() < 20 {
+            return TrainedModel::Uncommon;
+        }
+        let ((x_train, y_train), (x_test, y_test)) = self.split_data(0.92);
+        let len = x_train.len();
+        let x_train = DenseMatrix::from_2d_vec(&x_train);
+        let x_test = DenseMatrix::from_2d_vec(&x_test);
+        let (y_test_result, trained_model) = if len > 20000 {
+            println!("Training the model gaussian");
+            let trained_model =
+                GaussianNB::fit(&x_train, &y_train, GaussianNBParameters::default()).unwrap();
+            let y_test_result = trained_model.predict(&x_test).unwrap();
+            (y_test_result, TrainedModel::GaussianNB(trained_model))
+        } else if len > 2000 {
+            println!("Training the model binomial");
+            let trained_model =
+                BernoulliNB::fit(&x_train, &y_train, BernoulliNBParameters::default()).unwrap();
+            let y_test_result = trained_model.predict(&x_test).unwrap();
+            (y_test_result, TrainedModel::BernoulliNB(trained_model))
+        } else {
+            println!("Training the model random forest");
+            let trained_model = RandomForestClassifier::fit(
+                &x_train,
+                &y_train,
+                RandomForestClassifierParameters::default()
+                    .with_n_trees(100)
+                    .with_min_samples_leaf(1)
+                    .with_criterion(SplitCriterion::Entropy)
+                    .with_max_depth(100)
+                    .with_m(50),
+            )
+            .unwrap();
+            let y_test_result = trained_model.predict(&x_test).unwrap();
+            (y_test_result, TrainedModel::RandomForest(trained_model))
+        };
+
+        println!("Finishing the training");
+
+        let true_positive: u16 = y_test
+            .iter()
+            .zip(&y_test_result)
+            .map(|(&x, &y)| (x == y && x != 0) as u16)
+            .sum();
+
+        let false_negative: u16 = y_test
+            .iter()
+            .zip(&y_test_result)
+            .map(|(&x, &y)| (x != y && y == 0) as u16)
+            .sum();
+
+        let false_positive: u16 = y_test
+            .iter()
+            .zip(&y_test_result)
+            .map(|(&x, &y)| (x != y && y != 0) as u16)
+            .sum();
+
+        let true_negative: u16 = y_test
+            .iter()
+            .zip(&y_test_result)
+            .map(|(&x, &y)| (x == y && x == 0) as u16)
+            .sum();
+
+        println!(
+            "{} {} {} {}",
+            true_positive, true_negative, false_positive, false_negative
+        );
+
+        let total = true_negative + false_positive + false_negative + true_positive;
+        println!(
+            "Accuracy: {}",
+            (true_positive + true_negative) as f64 / total as f64
+        );
+
+        println!(
+            "F1 score {}",
+            (2.0 * true_positive as f64)
+                / (2.0 * true_positive as f64 + false_positive as f64 + false_negative as f64)
+        );
+        trained_model
+    }
+}
+
+enum TrainedModel {
+    RandomForest(RandomForestClassifier<f64, u8, DenseMatrix<f64>, Vec<u8>>),
+    BernoulliNB(BernoulliNB<f64, u8, DenseMatrix<f64>, Vec<u8>>),
+    GaussianNB(GaussianNB<f64, u8, DenseMatrix<f64>, Vec<u8>>),
+    Uncommon,
+}
+
+impl TrainedModel {
+    fn predict(&self, x: &[f64]) -> u8 {
+        let x = DenseMatrix::from_2d_array(&vec![x]);
+        match &self {
+            Self::RandomForest(model) => model.predict(&x),
+            Self::GaussianNB(model) => model.predict(&x),
+            Self::BernoulliNB(model) => model.predict(&x),
+            Self::Uncommon => panic!("PANIQUE"),
+        }
+        .unwrap()[0]
+    }
+}
+
+pub struct TrainedModels {
+    models: [[TrainedModel; 4]; 5],
+}
+
+impl TrainedModels {
+    pub fn predict(&self, rows: &[(Education, MaritalStatus, Vec<f64>)]) -> Vec<u8> {
+        let mut y = vec![];
+        for (education, marital_status, x) in rows {
+            let new_y = self.models[education.to_index()][marital_status.to_index()].predict(x);
+            y.push(new_y)
+        }
+        y
+    }
+}
+
+fn parse_row(row: &str) -> (Education, MaritalStatus, DataX, Option<DataY>) {
+    let mut result = DataX::default();
     let mut iter = row.split(',');
     iter.next(); //skip id
     result[0] = iter.next().unwrap().parse::<f64>().unwrap();
-    iter.next(); //skip sex
-    result[1] = education(iter.next().unwrap());
-    result[2] = status(iter.next().unwrap());
-    let other_data: [f64; 19] = array::from_fn(|_| iter.next().unwrap().parse::<f64>().unwrap());
-    result[3..].copy_from_slice(&other_data);
+    iter.next().unwrap(); //skip sex
+    let education = Education::new(iter.next().unwrap());
+    let marital_status = MaritalStatus::new(iter.next().unwrap());
+
+    let other_data: [f64; 19] = array::from_fn(|_| (iter.next().unwrap().parse::<f64>().unwrap()));
+    result[1..].copy_from_slice(&other_data);
     let y = iter.next().and_then(|x| x.parse::<u8>().ok());
-    (result, y)
+    (education, marital_status, result, y)
 }
 
-pub fn parse_rows() -> (Vec<Vec<f64>>, Vec<u8>) {
-    let file = include_str!("../data_participant.csv");
-    let mut lines = file.lines();
-    lines.next();
-    let mut x = vec![];
-    let mut y = vec![];
-    for row in lines {
-        let result = parse_row(row);
-        if let Some(data) = result.1 {
-            x.push(result.0.into_iter().collect());
-            y.push(data);
-        }
-    }
-    (x, y)
-}
-
-pub fn result() -> Vec<Vec<f64>> {
+pub fn result() -> Vec<(Education, MaritalStatus, Vec<f64>)> {
     let file = include_str!("../desjardins_presubmissions.csv");
     let mut lines = file.lines();
     lines.next();
-    let mut x = vec![];
+    let mut to_process = vec![];
     for row in lines {
-        let mut row: String = row.chars().skip_while(|&c| c != ',').skip(1).collect();
-        let result = parse_row(&row);
-        if let None = result.1 {
-            x.push(result.0.into_iter().collect());
-        }
+        let row: String = row.chars().skip_while(|&c| c != ',').skip(1).collect();
+        let (education, marital_status, x, _) = parse_row(&row);
+        to_process.push((education, marital_status, x.into_iter().collect()));
     }
-    x
+    to_process
 }
